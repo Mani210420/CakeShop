f1fe7211c81e48e0ab4e877a9e9bed8d8d6d73c6
Mani210420 <manikanta.kajjapu123@gmail.com>
2025-08-01 23:53:11 +0530

    Changes_05
    Made changes for repository

---
+++ CakeShop/Models/CakeRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace CakeShop.Models
+{
+    public class CakeRepository : ICakeRepository
+    {
+        private readonly CakeShopDbContext _cakeShopDbContext;
+
+        public CakeRepository(CakeShopDbContext cakeShopDbContext)
+        {
+            _cakeShopDbContext = cakeShopDbContext;
+        }
+
+        public IEnumerable<Cake> AllCakes => _cakeShopDbContext.Cakes.Include(c => c.Category) ;
+
+        public IEnumerable<Cake> CakeOfTheWeek
+        {
+            get
+            {
+                return _cakeShopDbContext.Cakes.Include(c => c.Category).Where(i => i.IsCakeOfTheWeek) ;
+            }
+        }
+
+        public Cake? GetCakeById(int Cakeid) => _cakeShopDbContext.Cakes.FirstOrDefault(c => c.CakeId == Cakeid);
+
+        public IEnumerable<Cake> SearchCake(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CakeShop/Models/CakeShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace CakeShop.Models
+{
+    public class CakeShopDbContext : DbContext
+    {
+        public CakeShopDbContext(DbContextOptions<CakeShopDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cake> Cakes {  get; set; }
+    }
+}
