ef5bd34aaf492e42e9b225cce12f0ae8722d009d
Mani210420 <manikanta.kajjapu123@gmail.com>
2025-08-05 22:05:38 +0530

    Changes_09

---
+++ CakeShop/Models/IShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿namespace CakeShop.Models
+{
+    public interface IShoppingCart
+    {
+        void AddToCart(Cake cake);
+        int RemoveFromCart(Cake cake);
+        List<ShoppingCartItem> shoppingCartItems();
+        void ClearCart();
+        decimal GetShoppingCartTotal();
+        List<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
